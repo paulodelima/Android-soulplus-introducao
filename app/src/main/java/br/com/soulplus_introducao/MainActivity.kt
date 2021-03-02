761fe24a2bcceee75911072b15536be927f27437
Paulo de Lima <paulodelimagp@gmail.com>
2021-03-02 16:51:42 -0400

    Enviando dados entre duas activities

--- app/src/main/java/br/com/soulplus_introducao/MainActivity.kt
+++ app/src/main/java/br/com/soulplus_introducao/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.soulplus_introducao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,17 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val tvResultadoTop = findViewById<TextView>(R.id.tvResultadoTop)
         val etNome = findViewById<EditText>(R.id.etNome)
         val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val btEnviar2 = findViewById<Button>(R.id.btEnviar2)
 
         btEnviar.setOnClickListener {
             if (etNome.text.isNotBlank()) {
-                tvResultado.text = getString(R.string.hello_name, etNome.text.toString())
+                tvResultadoTop.text = getString(R.string.hello_name, etNome.text.toString())
+            } else {
+                etNome.error = getString(R.string.type_your_name)
+                tvResultadoTop.text = ""
+            }
+        }
+
+        btEnviar2.setOnClickListener {
+            if (etNome.text.isNotBlank()) {
+                val nomeDigitado = etNome.text.toString()
+
+                val intent = Intent(this, ResultActivity::class.java)
+
+                intent.putExtra("NOME_DIGITADO", nomeDigitado)
+
+                startActivity(intent)
             } else {
                 etNome.error = getString(R.string.type_your_name)
             }
-
         }
 
 
